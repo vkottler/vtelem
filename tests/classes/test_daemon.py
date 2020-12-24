f084d8bf33da3b69dd38b7cfd9147046663ce7c0
Vaughn Kottler <vaughnkottler@gmail.com>
2020-12-24 04:38:43 -0600

    some more daemon testing

--- tests/classes/test_daemon.py
+++ tests/classes/test_daemon.py
@@ -16,6 +16,63 @@ def basic_task():
     print("test!")
 
 
+def test_daemon_callbacks():
+    """ TODO """
+
+    curr_time = float()
+    task_rate = 0.1
+
+    def sleep(sleep_time: float) -> None:
+        """ Example sleep function. """
+        nonlocal curr_time
+        curr_time += sleep_time
+        time.sleep(0.1)
+
+    def time_fn() -> float:
+        """ Example time-get function. """
+
+        return curr_time
+
+    def daemon_task(*args, **kwargs) -> None:
+        """ Example daemon task. """
+        assert args[0] == "arg1"
+        assert args[1] == "arg2"
+        assert args[2] == "arg3"
+        assert kwargs["kwarg1"] == 1
+        assert kwargs["kwarg2"] == 2
+        assert kwargs["kwarg3"] == 3
+        nonlocal curr_time
+        curr_time += 0.2
+        time.sleep(0.2)
+
+    def iter_overrun(start: float, end: float, rate: float,
+                     _: dict) -> None:
+        """ Example overrun function. """
+        assert end >= start
+        nonlocal task_rate
+        assert rate == task_rate
+
+    def iter_metrics(start: float, end: float, rate: float,
+                     _: dict) -> None:
+        """ Example metrics consumer. """
+        assert end >= start
+        nonlocal task_rate
+        assert rate == task_rate
+
+    def state_change(prev_state: DaemonState, curr_state: DaemonState) -> None:
+        """ Example state-change callback. """
+        assert prev_state != curr_state
+
+    daemon = Daemon(daemon_task, task_rate, time_fn, sleep, "test",
+                    iter_overrun, iter_metrics, state_change)
+    assert daemon.get_state() == DaemonState.IDLE
+    args = ["arg1", "arg2", "arg3"]
+    kwargs = {"kwarg1": 1, "kwarg2": 2, "kwarg3": 3}
+    assert daemon.start(*args, **kwargs)
+    time.sleep(0.5)
+    assert daemon.stop()
+
+
 def test_daemon_basic():
     """ Test basic daemon functionality. """
 
