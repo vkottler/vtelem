a56062f57fa30e52ca266ed283523292f5dfee59
Vaughn Kottler <Vaughn.Kottler@spacex.com>
2021-01-24 09:00:10 -0800

    filling in telemetry daemon

--- vtelem/classes/websocket_telemetry_daemon.py
+++ vtelem/classes/websocket_telemetry_daemon.py
@@ -4,12 +4,15 @@ vtelem - An interface for managing websocket servers that serve telemetry data.
 """
 
 # built-in
+import logging
 from typing import Any, Tuple
 
 # internal
 from .stream_writer import StreamWriter
 from .telemetry_environment import TelemetryEnvironment
 from .websocket_daemon import WebsocketDaemon
+
+LOG = logging.getLogger(__name__)
 
 
 class WebsocketTelemetryDaemon(WebsocketDaemon):
@@ -26,6 +29,15 @@ class WebsocketTelemetryDaemon(WebsocketDaemon):
             Write telemetry to this connection, for as long as it's connected.
             """
 
+            laddr = websocket.local_address
+            raddr = websocket.remote_address
+            conn = "opened"
+            fstr = "telemetry connection ('%s') %s '%s:%d' -> '%s:%d'"
+            LOG.info(fstr, path, conn, laddr[0], laddr[1], raddr[0], raddr[1])
+
+            while websocket.open:
+                pass
+
             # add the socket to the stream-writer, while loop on the connection
             # being open (we need to handle close signals somehow), once the
             # connection gets closed remove us from the stream-writer
@@ -33,5 +45,8 @@ class WebsocketTelemetryDaemon(WebsocketDaemon):
             print(websocket)
             print(path)
             print(writer)
+
+            conn = "closed"
+            LOG.info(fstr, path, conn, laddr[0], laddr[1], raddr[0], raddr[1])
 
         super().__init__(name, None, address, env, time_keeper, telem_handle)
